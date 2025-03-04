5ceb696287028608ccaa315f0f68b76e9173f861
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-03-03 17:35:06 -0700

    🎉 More nvim stuff

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local restore_cursor_augroup = vim.api.nvim_create_augroup("restore_cursor_shape_on_exit", { clear = true })
+vim.api.nvim_create_autocmd({"VimLeave"}, {
+    group = restore_cursor_augroup,
+    desc = "restore the cursor shape on exit of neovim",
+    command = "set guicursor=a:ver20",
+})
