f4b2b633b8555d52fa551f0629781a45ab716bcd
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-03-11 16:31:36 -0600

    More commits
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- init.lua
+++ init.lua
@@ -1,9 +1,18 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+-- Get rid of Neovim's stupid cursor change
 local restore_cursor_augroup = vim.api.nvim_create_augroup("restore_cursor_shape_on_exit", { clear = true })
 vim.api.nvim_create_autocmd({ "VimLeave" }, {
   group = restore_cursor_augroup,
   desc = "restore the cursor shape on exit of neovim",
   command = "set guicursor=a:ver20",
+})
+
+-- Get rid of Neovim's keybind for Ctrl+Z
+local unbind_ctrl_z = vim.api.nvim_create_augroup("unbind_ctrl_z__on_exit", { clear = true })
+vim.api.nvim_create_autocmd({ "VimLeave" }, {
+  group = unbind_ctrl_z,
+  desc = "Unbinds Ctrl+Z",
+  command = "nnoremap <C-Z> <Nop>",
 })
