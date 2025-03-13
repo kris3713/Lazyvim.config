5f41ad19d157b3f1019b6614b924d5467ff6fc9d
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-03-13 13:37:35 -0600

    Added a plugin for log files and updated config.
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- init.lua
+++ init.lua
@@ -1,28 +1,29 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+require('config.lazy')
+-- For Comment.nvim
+require('Comment').setup()
 
 -- Get rid of Neovim's stupid cursor change
-local restore_cursor_augroup = vim.api.nvim_create_augroup("restore_cursor_shape_on_exit", { clear = true })
-vim.api.nvim_create_autocmd({ "VimLeave" }, {
+local restore_cursor_augroup = vim.api.nvim_create_augroup('restore_cursor_shape_on_exit', { clear = true })
+vim.api.nvim_create_autocmd({ 'VimLeave' }, {
   group = restore_cursor_augroup,
-  desc = "restore the cursor shape on exit of neovim",
-  command = "set guicursor=a:ver20",
+  desc = 'restore the cursor shape on exit of neovim',
+  command = 'set guicursor=a:ver20',
 })
 
--- Set font family and font size
-vim.opt.guifont = { "JetBrainsMono Nerd Font", ":h16" }
+-- Set font family and font size (For Neovide and Neovim-Qt)
+vim.opt.guifont = { 'JetBrainsMono Nerd Font', ':h16' }
+-- Set softwrapping to always be true
+vim.opt.wrap = true
 
 -- Set zoom function
 vim.g.neovide_scale_factor = 1.0
 local change_scale_factor = function(delta)
   vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
 end
-vim.keymap.set("n", "<C-=>", function()
+vim.keymap.set('n', '<C-=>', function()
   change_scale_factor(1.25)
 end)
-vim.keymap.set("n", "<C-->", function()
+vim.keymap.set('n', '<C-->', function()
   change_scale_factor(1/1.25)
 end)
-
--- For the Comment.nvim
-require('Comment').setup()
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,30 +1,42 @@
 return {
-  {
-    "roobert/surround-ui.nvim",
+  { -- Set colorscheme
+    'LazyVim/LazyVim',
+    opts = {
+      colorscheme = 'catppuccin-macchiato'
+    }
+  },
+  { -- Activate roobert/surround-ui.nvim
+    'roobert/surround-ui.nvim',
     dependencies = {
-      "kylechui/nvim-surround",
-      "folke/which-key.nvim",
+      'kylechui/nvim-surround',
+      'folke/which-key.nvim',
     },
     config = function()
-      require("surround-ui").setup({
-        root_key = "S"
+      require('surround-ui').setup({
+        root_key = 'S'
       })
     end
   },
-  {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
+  { -- Activate kylechui/nvim-surround
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
     config = function()
-      require("nvim-surround").setup({
+      require('nvim-surround').setup({
         -- Configuration here, or leave empty to use defaults
       })
     end
   },
-  {
+  { -- Activate numToStr/Comment
     'numToStr/Comment.nvim',
     opts = {
       -- add any options here
     }
+  },
+  { -- Set syntax highlighting for logs
+    'fei6409/log-highlight.nvim',
+    config = function()
+      require('log-highlight').setup {}
+    end
   }
 }
