f3b6c99dc2d3260dbcf194504145144ebe400d8c
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-03-13 16:19:15 -0600

    Removed key map for quit command.
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
--- for :q command
-vim.keymap.set('n', '<c-q>', ':q<cr>', { desc = 'quit neovim' })
