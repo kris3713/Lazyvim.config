c3a3715e550208afd4a4520e6bd61af1c6f3e967
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-03-13 17:40:48 -0600

    Added csharp.nvim plugin
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -48,5 +48,17 @@ return {
     ---@module 'ibl'
     ---@type ibl.config
     opts = {}
+  },
+  {
+    "iabdelkareem/csharp.nvim",
+    dependencies = {
+      "williamboman/mason.nvim", -- Required, automatically installs omnisharp
+      "mfussenegger/nvim-dap",
+      "Tastyep/structlog.nvim" -- Optional, but highly recommended for debugging
+    },
+    config = function ()
+      require("mason").setup() -- Mason setup must run before csharp, only if you want to use omnisharp
+      require("csharp").setup()
+    end
   }
 }
