4d1ca7505cb5435c88dabe4d2f408154f577ab54
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-03-13 16:02:45 -0600

    Updates and stuff
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- init.lua
+++ init.lua
@@ -4,7 +4,9 @@ require('config.lazy')
 require('Comment').setup()
 
 -- Get rid of Neovim's stupid cursor change
-local restore_cursor_augroup = vim.api.nvim_create_augroup('restore_cursor_shape_on_exit', { clear = true })
+local restore_cursor_augroup = vim.api.nvim_create_augroup('restore_cursor_shape_on_exit', {
+  clear = true
+})
 vim.api.nvim_create_autocmd({ 'VimLeave' }, {
   group = restore_cursor_augroup,
   desc = 'restore the cursor shape on exit of neovim',
@@ -15,18 +17,16 @@ vim.api.nvim_create_autocmd({ 'VimLeave' }, {
 vim.opt.guifont = { 'JetBrainsMono Nerd Font', ':h16' }
 -- Set softwrapping to always be true
 vim.opt.wrap = true
+-- Set hovering to enable
+vim.lsp.buf.hover()
 
 -- Set zoom function
 vim.g.neovide_scale_factor = 1.0
 local change_scale_factor = function(delta)
   vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
 end
-vim.keymap.set('n', '<C-=>', function()
-  change_scale_factor(1.25)
-end)
-vim.keymap.set('n', '<C-->', function()
-  change_scale_factor(1/1.25)
-end)
+vim.keymap.set('n', '<c-=>', function() change_scale_factor(1.25) end)
+vim.keymap.set('n', '<c-->', function() change_scale_factor(1/1.25) end)
 
 -- rainbow delimiters
 -- This module contains a number of default definitions
