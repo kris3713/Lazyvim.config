e24b75b550b6aa950736d7ff839698df064f5e9d
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-04-03 15:51:47 -0600

    Might try Nvimtree instead of Neotree later on

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -79,3 +79,10 @@ vim.o.foldcolumn = '1' -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -92,13 +92,7 @@ return {
       -- if you want to open yazi instead of netrw, see below for more info
       open_for_directories = true,
       -- keymaps = { show_help = '<f1>' }
-    },
-    -- 👇 if you use `open_for_directories=true`, this is recommended
-    init = function()
-      -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
-      -- vim.g.loaded_netrw = 1
-      vim.g.loaded_netrwPlugin = 1
-    end
+    }
   },
   {
     'soulis-1256/eagle.nvim',
