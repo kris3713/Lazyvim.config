500393cf4d517544f4ce8faced69d9a1053b429b
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-04-08 22:21:06 -0600

    Update `lua/config/autocmds.lua`
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -75,3 +75,16 @@ vim.api.nvim_create_autocmd('VimEnter', {
     require('nvim-tree.api').tree.open()
   end
 })
+
+-- nvim-tree workaround when using rmagatti/auto-session
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+  pattern = 'NvimTree*',
+  callback = function()
+    local api = require('nvim-tree.api')
+    local view = require('nvim-tree.view')
+
+    if not view.is_visible() then
+      api.tree.open()
+    end
+  end,
+})
