50cda4562c6c95cfa9628bd5423e127c00a07b7b
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-06-18 11:47:26 -0600

    Update 06-18-2025 -- Part 3
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -93,7 +93,6 @@ return {
     '3rd/image.nvim',
     build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
     opts = {
-      backend = 'wezterm',
       processor = 'magick_cli'
     }
   },
