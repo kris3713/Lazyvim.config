f827cc28d24dbb26ed814bb93bcb44f55c09df21
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-06-28 01:14:36 -0600

    Update 06-28-2025 -- Part 1
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -228,6 +228,16 @@ return {
     end
   },
   {
+    'michaelb/sniprun',
+    branch = 'master',
+    build = 'sh ./install.sh',
+    -- do 'sh install.sh 1' if you want to force compile locally
+    -- (instead of fetching a binary from the github release). Requires Rust >= 1.65
+    config = function()
+      require('sniprun').setup {}
+    end
+  },
+  {
     'mcauley-penney/visual-whitespace.nvim',
     event = 'ModeChanged *:[vV\22]',
     opts = {
