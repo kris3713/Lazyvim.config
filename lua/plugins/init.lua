5ab7ffa9b9f1e7a6d10dfa66387e66a597be3667
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-07-17 22:36:46 -0600

    Update 07-17-2025 -- Part 5
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -327,36 +327,34 @@ return {
   },
   {
     'milanglacier/minuet-ai.nvim',
-    config = function()
-      require('minuet').setup {
-        provider = 'gemini',
-        provider_options = {
-          gemini = {
-            model = 'gemini-2.0-flash',
-            optional = {
-              generationConfig = {
-                maxOutputTokens = 256,
-                -- When using `gemini-2.5-flash`, it is recommended to entirely
-                -- disable thinking for faster completion retrieval.
-                thinkingConfig = {
-                  thinkingBudget = 0
-                }
-              },
-              safetySettings = {
-                {
-                  -- HARM_CATEGORY_HATE_SPEECH,
-                  -- HARM_CATEGORY_HARASSMENT
-                  -- HARM_CATEGORY_SEXUALLY_EXPLICIT
-                  category = 'HARM_CATEGORY_DANGEROUS_CONTENT',
-                  -- BLOCK_NONE
-                  threshold = 'BLOCK_ONLY_HIGH'
-                }
+    opts = {
+      provider = 'gemini',
+      provider_options = {
+        gemini = {
+          model = 'gemini-2.0-flash',
+          optional = {
+            generationConfig = {
+              maxOutputTokens = 256,
+              -- When using `gemini-2.5-flash`, it is recommended to entirely
+              -- disable thinking for faster completion retrieval.
+              thinkingConfig = {
+                thinkingBudget = 0
+              }
+            },
+            safetySettings = {
+              {
+                -- HARM_CATEGORY_HATE_SPEECH,
+                -- HARM_CATEGORY_HARASSMENT
+                -- HARM_CATEGORY_SEXUALLY_EXPLICIT
+                category = 'HARM_CATEGORY_DANGEROUS_CONTENT',
+                -- BLOCK_NONE
+                threshold = 'BLOCK_ONLY_HIGH'
               }
             }
           }
         }
       }
-    end
+    }
   },
   {
     'yetone/avante.nvim',
