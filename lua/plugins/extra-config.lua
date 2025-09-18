a59189a99404a246c78f22018fbf4eb303226e46
kris3713 <07e5297d5b@c0x0.com>
2025-09-17 20:00:27 -0600

    PR https://github.com/LazyVim/LazyVim/pull/6354 has been merged, removing unneeded init property
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

--- lua/plugins/extra-config.lua
+++ lua/plugins/extra-config.lua
@@ -53,11 +53,6 @@ return {
   },
   {
     'akinsho/bufferline.nvim',
-    -- TODO: Remove this once https://github.com/LazyVim/LazyVim/pull/6354 is merged
-    init = function()
-      local bufline = require('catppuccin.groups.integrations.bufferline')
-      bufline.get = bufline.get_theme
-    end,
     ---@module 'bufferline'
     ---@type bufferline.Config
     opts = {
