3aecf2e0b355c4958085a4152b42eebd056b78aa
kris3713 <07e5297d5b@c0x0.com>
2025-11-12 12:40:08 -0700

    Update
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Enable 24 bit color
+vim.opt.termguicolors = true
+
 -- Remove annoying autoformat
 vim.g.autoformat = false
 
