7efb295ba4bdbd39e60bdc9cbdce9ab74d0e0607
kris3713 <07e5297d5b@c0x0.com>
2026-01-03 20:58:53 -0700

    Updates
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

--- lua/plugins/extra-config.lua
+++ lua/plugins/extra-config.lua
@@ -527,7 +527,13 @@ return {
         code_actions.statix,
         code_actions.ts_node_action.with {
           -- Don't remove, this is needed by `nvim-tree.lua`
-          disabled_filetypes = { 'spec', 'conf', 'dosini', 'systemd' }
+          disabled_filetypes = {
+            'spec',
+            'conf',
+            'dosini',
+            'systemd',
+            'fish'
+          }
         },
         diagnostics.deadnix,
         diagnostics.dotenv_linter,
