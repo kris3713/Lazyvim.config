3dffc05c2a0624692cb1b1ea2e620ea4c19f6816
kris3713 <07e5297d5b@c0x0.com>
2026-01-10 18:04:45 -0700

    Removed nvim-aider plugin
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -18,6 +18,48 @@ return {
   }
   -- harper:ignore
   -- {
+  --   'GeorgesAlkhouri/nvim-aider',
+  --   cmd = 'Aider',
+  --   -- Example key mappings for common actions:
+  --   keys = {
+  --     { '<leader>a/', '<cmd>Aider toggle<cr>', desc = 'Toggle Aider' },
+  --     { '<leader>as', '<cmd>Aider send<cr>', desc = 'Send to Aider', mode = { 'n', 'v' } },
+  --     { '<leader>ac', '<cmd>Aider command<cr>', desc = 'Aider Commands' },
+  --     { '<leader>ab', '<cmd>Aider buffer<cr>', desc = 'Send Buffer' },
+  --     { '<leader>a+', '<cmd>Aider add<cr>', desc = 'Add File' },
+  --     { '<leader>a-', '<cmd>Aider drop<cr>', desc = 'Drop File' },
+  --     { '<leader>ar', '<cmd>Aider add readonly<cr>', desc = 'Add Read-Only' },
+  --     { '<leader>aR', '<cmd>Aider reset<cr>', desc = 'Reset Session' },
+  --     -- Example nvim-tree.lua integration if needed
+  --     {
+  --       '<leader>a+',
+  --       function()
+  --         require('nvim_aider.tree').add_file_from_tree()
+  --       end,
+  --       desc = 'Add File from Tree to Aider',
+  --       ft = 'NvimTree'
+  --     },
+  --     {
+  --       '<leader>a-',
+  --       function()
+  --         require('nvim_aider.tree').drop_file_from_tree()
+  --       end,
+  --       desc = 'Drop File from Tree from Aider',
+  --       ft = 'NvimTree'
+  --     },
+  --   },
+  --   ---@type nvim_aider.Config
+  --   opts = {
+  --     args = {
+  --       '--model',
+  --       'gemini/gemini-2.5-flash',
+  --       '--no-auto-commits',
+  --       '--pretty',
+  --       '--stream'
+  --     }
+  --   }
+  -- }
+  -- {
   --   'lewis6991/hover.nvim',
   --   ---@type Hover.Config
   --   opts = {
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -545,48 +545,6 @@ return {
     end
   },
   {
-    'GeorgesAlkhouri/nvim-aider',
-    cmd = 'Aider',
-    -- Example key mappings for common actions:
-    keys = {
-      { '<leader>a/', '<cmd>Aider toggle<cr>', desc = 'Toggle Aider' },
-      { '<leader>as', '<cmd>Aider send<cr>', desc = 'Send to Aider', mode = { 'n', 'v' } },
-      { '<leader>ac', '<cmd>Aider command<cr>', desc = 'Aider Commands' },
-      { '<leader>ab', '<cmd>Aider buffer<cr>', desc = 'Send Buffer' },
-      { '<leader>a+', '<cmd>Aider add<cr>', desc = 'Add File' },
-      { '<leader>a-', '<cmd>Aider drop<cr>', desc = 'Drop File' },
-      { '<leader>ar', '<cmd>Aider add readonly<cr>', desc = 'Add Read-Only' },
-      { '<leader>aR', '<cmd>Aider reset<cr>', desc = 'Reset Session' },
-      -- Example nvim-tree.lua integration if needed
-      {
-        '<leader>a+',
-        function()
-          require('nvim_aider.tree').add_file_from_tree()
-        end,
-        desc = 'Add File from Tree to Aider',
-        ft = 'NvimTree'
-      },
-      {
-        '<leader>a-',
-        function()
-          require('nvim_aider.tree').drop_file_from_tree()
-        end,
-        desc = 'Drop File from Tree from Aider',
-        ft = 'NvimTree'
-      },
-    },
-    ---@type nvim_aider.Config
-    opts = {
-      args = {
-        '--model',
-        'gemini/gemini-2.5-flash',
-        '--no-auto-commits',
-        '--pretty',
-        '--stream'
-      }
-    }
-  },
-  {
     'LunarVim/bigfile.nvim',
     config = function()
       local supermaven = {
