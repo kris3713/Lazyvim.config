9214771421a244cf8a1ee209dfc502d7a696d77b
kris3713 <07e5297d5b@c0x0.com>
2026-03-10 11:45:34 -0600

    Updates
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -706,10 +706,10 @@ return --[[@type (LazyPluginSpec[])]]{
       context_window = 512,
       provider_options = {
         openai_fim_compatible = {
-          api_key = 'TERM',
+          api_key = 'ollama',
           name = 'Ollama',
           end_point = 'http://localhost:11434/v1/completions',
-          model = 'qwen2.5-coder:7b-instruct-q4_K_M',
+          model = 'qwen2.5-coder:14b-instruct-q4_K_M',
           optional = {
             max_tokens = 56,
             top_p = 0.9
