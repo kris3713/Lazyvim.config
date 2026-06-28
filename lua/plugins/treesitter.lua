28c275aebd14a1800dd6284d6627569692d20770
kris3713 <07e5297d5b@c0x0.com>
2026-06-27 19:57:33 -0600

    Added keymaps for treewalker plugin
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -14,6 +14,79 @@ return  --[[@type (LazyPluginSpec[])]]{
   {
     'aaronik/treewalker.nvim',
     opts = {},
+    ---@param keys LazyKeysSpec[]|LazyKeys[]
+    keys = function(_, keys)
+      local modes = { 'n', 'v' }
+
+      keys = {
+        {
+          '<c-k>',
+          function()
+            vim.cmd('Treewalker Up')
+          end,
+          desc = 'Treewalker Up',
+          mode = modes,
+        },
+        {
+          '<c-j>',
+          function()
+            vim.cmd('Treewalker Down')
+          end,
+          desc = 'Treewalker Down',
+          mode = modes,
+        },
+        {
+          '<c-h>',
+          function()
+            vim.cmd('Treewalker Left')
+          end,
+          desc = 'Treewalker Left',
+          mode = modes,
+        },
+        {
+          '<c-l>',
+          function()
+            vim.cmd('Treewalker Right')
+          end,
+          desc = 'Treewalker Right',
+          mode = modes,
+        },
+        {
+          '<c-s-k>',
+          function()
+            vim.cmd('Treewalker SwapUp')
+          end,
+          desc = 'Treewalker SwapUp',
+          mode = 'n',
+        },
+        {
+          '<c-s-j>',
+          function()
+            vim.cmd('Treewalker SwapDown')
+          end,
+          desc = 'Treewalker SwapDown',
+          mode = 'n',
+        },
+        {
+          '<c-s-h>',
+          function()
+            vim.cmd('Treewalker SwapLeft')
+          end,
+          desc = 'Treewalker SwapLeft',
+          mode = 'n',
+        },
+        {
+          '<c-s-l>',
+          function()
+            vim.cmd('Treewalker SwapRight')
+          end,
+          desc = 'Treewalker SwapRight',
+          mode = 'n',
+        },
+      }
+
+      return keys
+    end,
   },
   {
     'Sang-it/fluoride',
