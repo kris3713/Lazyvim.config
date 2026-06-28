22171f268db06c220fbf317d0c3433b8541f49fa
kris3713 <07e5297d5b@c0x0.com>
2026-06-27 21:44:29 -0600

    Update lua/plugins/lsp-configs.lua
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

--- lua/plugins/lsp-configs.lua
+++ lua/plugins/lsp-configs.lua
@@ -868,59 +868,6 @@ return  --[[@type LazyPluginSpec]]{
           end)
           -- end workaround
         end,
-        vtsls = function(_, _)
-          Snacks.util.lsp.on({ name = 'vtsls' }, function(_, client)
-            client.commands['_typescript.moveToFileRefactoring'] = function(command, _)
-              ---@type string, string, lsp.Range
-              local action, uri, range = unpack(command.arguments)
-
-              --- @diagnostic disable-next-line: incomplete-signature-doc
-              local function move(newf)
-                client:request('workspace/executeCommand', {
-                  command = command.command,
-                  arguments = { action, uri, range, newf },
-                })
-              end
-
-              local fname = vim.uri_to_fname(uri)
-              client:request('workspace/executeCommand', {
-                command = 'typescript.tsserverRequest',
-                arguments = {
-                  'getMoveToRefactoringFileSuggestions',
-                  {
-                    file = fname,
-                    startLine = range.start.line + 1,
-                    startOffset = range.start.character + 1,
-                    endLine = range['end'].line + 1,
-                    endOffset = range['end'].character + 1,
-                  },
-                },
-              }, function(_, result)
-                ---@type string[]
-                local files = result.body.files
-                table.insert(files, 1, 'Enter new path...')
-                vim.ui.select(files, {
-                  prompt = 'Select move destination:',
-                  format_item = function(f)
-                    return vim.fn.fnamemodify(f, ':~:.')
-                  end,
-                }, function(f)
-                  if f and f:find('^Enter new path') then
-                    vim.ui.input({
-                      prompt = 'Enter move destination:',
-                      default = vim.fn.fnamemodify(fname, ':h') .. '/',
-                      completion = 'file',
-                    }, function(newf)
-                      return newf and move(newf)
-                    end)
-                  elseif f then
-                    move(f)
-                  end
-                end)
-              end)
-            end
-          end)
-        end,
       },
     }
 
